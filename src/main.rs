e596b4608f1037859bbe4492b58301855188fd49
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-06-30 06:23:14 +0530

    Guessing game logic implemented

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,48 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Welcome to the game");
-    println!("Please enter a number");
 
-    let mut guess = String::new();
+    // let is a constant variable
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    // Print the secret number
+    println!("The secret number is {}", secret_number);
 
-    println!("You guessed: {}", guess);
+    loop {
+        println!("Guess the number");
+
+        // &mut is a reference to a mutable variable
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // parse the string to an integer
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        // Print the guessed number
+        println!("You guessed: {}", guess);
+        // Compare the guessed number to the secret number
+        match guess.cmp(&secret_number) {
+            // If the guessed number is equal to the secret number
+            std::cmp::Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+            // If the guessed number is less than the secret number
+            std::cmp::Ordering::Less => {
+                println!("Too small!");
+            }
+            // If the guessed number is greater than the secret number
+            std::cmp::Ordering::Greater => {
+                println!("Too big!");
+            }
+        }
+    }
 }
